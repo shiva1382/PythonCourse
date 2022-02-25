1ae87f0ae95bfb8fc4078fb007c39a257939726e
shiva1382 <99687365+shiva1382@users.noreply.github.com>
2022-02-25 19:53:20 +0330

    Add files via upload

---
+++ HomeWork2.py
@@ -0,0 +1,65 @@
+# question 6:
+x=2
+print('question 6')
+print("x")
+print('x')
+print(x)
+print("x + 1")
+# print('x'+ 1) # error
+print(x + 1)
+#question 7:
+i1 = 2
+i2 = 5
+i3 = -3
+d1 = 2.0
+d2 = 5.0
+d3 = -0.5
+print('question 7')
+print(i1 + i2)
+print(i1 / i2)
+print(i1 // i2)
+print(i2 / i1)
+print(i2 // i1)
+print(i1 * i3)
+print(d1 + d2)
+print(d1 / d2)
+print(d2 / d1)
+print(d3 * d1)
+print(d1 + i2)
+print(i1 / d2)
+print(d2 / i1)
+print(i2 / d1)
+print(i1/i2*d1)
+print(d1*i1/i2)
+print(d1/d2*i1)
+print(i1*d1/d2)
+print(i2/i1*d1)
+print(d1*i2/i1)
+print(d2/d1*i1)
+print(i1*d2/d1)
+#question 9:
+i1 = 2
+i2 = 5
+i3 = -3
+d1 = 2.0
+d2 = 5.0
+d3 = -0.5
+print('question 9')
+print(i1 + (i2 * i3))
+print(i1 * (i2 + i3))
+print(i1 / (i2 + i3))
+print(i1 // (i2 + i3))
+print(i1 / i2 + i3)
+print(i1 // i2 + i3)
+print((3 + 4 + 5) / 3)
+print((3 + 4 + 5) // 3)
+print((3 + 4 + 5) / 3)
+print((3 + 4 + 5) // 3)
+print(d1 + (d2 * d3))
+print(d1 + d2 * d3)
+print(d1 / d2 - d3)
+print(d1 / (d2 - d3))
+print(d1 + d2 + d3 / 3)
+print((d1 + d2 + d3) / 3)
+print(d1 + d2 + (d3 / 3))
+print(3 * (d1 + d2) * (d1 - d3))
