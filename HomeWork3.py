7498860e32ee9e603c859412f82ed95c495b6714
shiva1382 <99687365+shiva1382@users.noreply.github.com>
2022-03-05 17:19:36 +0330

    Create HomeWork3.py

---
+++ HomeWork3.py
@@ -0,0 +1,10 @@
+ #question 3:
+  input 1 = in(input("enter a number  : "))
+  input 2 = in(input("enter a number  : "))
+  input 3 = in(input("enter a number  : "))
+  input 4 = in(input("enter a number  : "))
+  input 5 = in(input("enter a number  : "))
+  if input1 == input2 or input1 == input3 or input1 == input4 or input1 == input5:
+  else input2 == input3 or input2 == input4 or input2 == input5 or input3 == input4 or input3 == input5 or input4 == input5:
+  else:
+   print("All unique") 
