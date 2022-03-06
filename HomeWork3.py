7c75743bc68d1074dc816eb090fc747fa2d9d05c
shiva1382 <99687365+shiva1382@users.noreply.github.com>
2022-03-06 21:59:51 +0330

    Update HomeWork3.py

--- HomeWork3.py
+++ HomeWork3.py
@@ -1,3 +1,35 @@
+#question1
+salary = int(input('Enter Salary :'))
+if salary <= 1000:
+    taxrate = 0
+    tax = salary * 0 / 100
+    netincome = salary - tax
+    print("taxrate=", taxrate, "tax=", tax, "netincome=", netincome)
+if salary > 1000:
+    if salary <= 2500:
+        taxrate = 10
+        tax = salary * 10 / 100
+        netincome = salary - tax
+        print("taxrate=", taxrate, "tax=", tax, "netincome=", netincome)
+if salary > 2500:
+    if salary <= 4000:
+        taxrate = 15
+        tax = salary * 15 / 100
+        netincome = salary - tax
+        print("taxrate=", taxrate, "tax=", tax, "netincome=", netincome)
+if salary > 4000:
+    if salary <= 6000:
+        taxrate = 20
+        tax = salary * 20 / 100
+        netincome = salary - tax
+        print("taxrate=", taxrate, "tax=", tax, "netincome=", netincome)
+if salary > 8000:
+    taxrate = 30
+    tax = salary * 30 / 100
+    netincome = salary - tax
+    print("taxrate=", taxrate, "tax=", tax, "netincome=", netincome)
+
+
 #question 2:
 firstNum = int(input("enter first number :")
 secondNum = int(input("enter second number :")
